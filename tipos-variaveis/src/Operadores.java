510f724e4d424bb5a5112f332c50b1802b258089
devvMiguel <miguelgalvao_galvao@hotmail.com>
2024-05-24 17:32:07 -0300

    Aprendi sobre os tipos de variáveis que existem e os operadores matemáticos na sintaxe Java.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        Boolean simNao = a == b;
+
+        System.out.println("A é igual a B? "+simNao);
+    }
+    
+}
